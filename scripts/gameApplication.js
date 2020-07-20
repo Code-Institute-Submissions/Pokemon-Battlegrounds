14c76caa71e0c37adfc99a5779d537671f9f911b
l0nelyhermit <mrjonathangoh@gmail.com>
2020-07-20 15:53:51 +0800

    tweaked functionality for real-time gps tracking

--- scripts/gameApplication.js
+++ scripts/gameApplication.js
@@ -12,39 +12,34 @@ $(function(){
                 zoomOffset: -1,
                 accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
             }).addTo(displayMap);
-               let playerMarker = L.marker(user_position).addTo(displayMap);
+
+               let playerMarker = L.marker(user_position)
+               playerMarker.addTo(displayMap);
+
+               displayMap.locate({
+                   watch:true,
+                   maxZoom:20,
+                   setView: true
+               })
+
+               function foundLocation(e){
+                   if(playerMarker){
+                       playerMarker.removeLayer(playerMarker);
+                   }
+                   let newPlayerMarker = L.marker(e.latlng).addTo(displayMap)
+               }
+
+               displayMap.on('locationfound', foundLocation)
+
+
             })
         }else{
             alert('Geolocation service has failed, unable to retrieve your location')
             }
         }
+    
 
-        function updateUserPosition(){
-            function foundLocation(e){
-                if(playerMarker){
-                    displayMap.removeLayer(playerMarker);
-                }
-                playerMarker = L.marker(e.latitude,e.longitude);
-                playerMarker.addTo(displayMap);
-            }
-
-            function ErrorLocation(){
-                alert('Error in retrieving your location')
-            }
-
-            displayMap.on('locationfound',foundLocation);
-            displayMap.on('locationerror',ErrorLocation);
-
-            displayMap.locate({
-                watch: true,
-                setView:true,
-                maxZoom: 20,
-                enableHighAccuracy: true
-            }
-            )
-        }
 
     initializeMapWithUserPosition()
-    updateUserPosition()
  
 })
